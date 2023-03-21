663809a06fe8d11452c74f1e116b97a1057b4a83
Ștefan Popescu <stefan.popescu@student.fontys.nl>
2023-03-21 12:58:40 +0100

    For real now

--- EventixWebApp/Eventix Wrapped Up/Entities/User.cs
+++ EventixWebApp/Eventix Wrapped Up/Entities/User.cs
@@ -1,6 +1,33 @@
 ﻿namespace Eventix_Wrapped_Up.Entities
 {
-    public class Client
+
+    public class User
     {
+        private string uuid;
+        private string name;
+        private string password;
+        private string email;
+        private string phone;
+
+        public User()
+        {
+            
+        }
+        public User(string uuid, string name, string password, string email, string phone)
+        {
+            this.uuid = uuid;
+            this.name = name;
+            this.password = password;
+            this.email = email;
+            this.phone = phone;
+        }
+
+        public User(string name, string password, string email, string phone)
+        {
+            this.name = name;
+            this.password = password;
+            this.email = email;
+            this.phone = phone;
+        }
     }
 }
