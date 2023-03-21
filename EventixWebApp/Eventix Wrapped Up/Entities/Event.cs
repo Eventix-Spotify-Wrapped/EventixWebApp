161c426843f7a1cfcd8de33c4c46fa2eb6be6231
Andrei Sava <andisavauserul@gmail.com>
2023-03-21 13:29:45 +0100

    Event class was created within this commit. Thank you.

---
+++ EventixWebApp/Eventix Wrapped Up/Entities/Event.cs
@@ -0,0 +1,31 @@
+﻿namespace Eventix_Wrapped_Up.Entities
+{
+    public class Event
+    {
+        private string uuid;
+        private string name;
+        private EventType type;
+        private EventStatus status;
+        private string locale; //event locale (en_GB, etc.)
+        private Currency currency;
+    }
+
+    public enum EventType
+    {
+        ONCE,
+        REPEATING,
+    }
+    public enum EventStatus
+    {
+        UNAVAILABLE,
+        AVAILABLE,
+        SOLD_OUT,
+        REFUNDING,
+        MOVING,
+    }
+    public enum Currency
+    {
+        EUR,
+        USD,
+    }
+}
