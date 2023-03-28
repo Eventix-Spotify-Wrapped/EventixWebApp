b338ed383b0bdfdccd1d8a9287561a00a8e5d8cd
Ștefan Popescu <stefan.popescu@student.fontys.nl>
2023-03-28 14:56:58 +0200

    Added the new project python base

--- Eventix Wrapped Up/webapi/Entities/Shop.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Eventix_Wrapped_Up.Entities
-{
-    public class Shop
-    {
-        private string guid;
-        private string name;
-    }
-}
--- Eventix Wrapped Up/webapi/Entities/Statistics.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Eventix_Wrapped_Up.Entities
-{
-    public class Statistics
-    {
-        //do actions with Statistics endpoint
-    }
-}
