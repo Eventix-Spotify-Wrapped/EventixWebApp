8a2b08f778b35eea1465d9a395b73236b99cb7c9
Andrei Sava <andisavauserul@gmail.com>
2023-03-30 11:47:39 +0200

    Renamed eventix django project and created eventix django application.

---
+++ EventixPrj/EventixApp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EventixappConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "EventixApp"
--- EventixPrj/EventixPrj/asgi.py
+++ EventixPrj/EventixPrj/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for APIEventix project.
+ASGI config for EventixPrj project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'APIEventix.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "EventixPrj.settings")
 
 application = get_asgi_application()
