91b528016e159acad54d9ff2bc6ed03aed8c2ff2
rubenvandeven01@gmail.com <rubenvandeven01@gmail.com>
2023-05-19 00:00:39 +0200

    Plug animation at begin slide

--- EventixPrj/EventixApp/resources/js/main.js
+++ EventixPrj/EventixApp/resources/js/main.js
@@ -1,5 +1,4 @@
 // Variables
-const amountOfSlide = 7;
 const slideColors = [
     {
         main: "#ED7CB8",
@@ -26,77 +25,85 @@ let app = document.getElementById("app");
 let slides = document.getElementsByClassName("slide");
 let coins = [document.getElementsByClassName("coin")[0]];
 let findTheTruth = document.getElementById("find-the-truth");
+const amountOfSlides = slides.length;
 
-// Add all coins to array
-for (let i = 0; i < (amountOfSlide - 1); i++) coins.push( app.appendChild(coins[0].cloneNode(true)));
-
-// When the slot machine-handle is clicked
-document.getElementById("machine").addEventListener("click", () => {
-
-    // Pull the lever downwards with a CSS class
-    document.getElementsByClassName("lever")[0].classList.add("lever--active");
-
-    // Animate the bottom slide
-    document.getElementsByClassName("slide__backfigure")[0].classList.add("slide__backfigure--visible");
-
-    // Calculate the left value so the coins are centered
-    let coinsWidth = amountOfSlide * 4;
-    let leftRemValue = (36 - coinsWidth) / 2 - 0.4;
+slides[0].addEventListener("click", () => {
+    document.getElementById("plug").classList.add("active");
+    setTimeout(() => document.getElementsByClassName("begin-illustration")[0].classList.add("begin-illustration--start"), 1000);
 
     setTimeout(() => {
+        // Pull the lever downwards with a CSS class
+        document.getElementsByClassName("lever")[0].classList.add("lever--active");
 
-        // Position every coin centered at the bottom of the screen
-        for (let i = 0; i < coins.length; i++) {
-            setTimeout(() => {
-                let coin = coins[i];
-                coin.style.top = 67 + "rem";
-                coin.style.left = leftRemValue + "rem";
-        
-                leftRemValue += 4;
-        
-                // Let every second coin slide down so they are paired
-                if (i % 2) coin.style.top = (parseInt(coin.style.top.slice(0, -3)) + 1.25) + "rem";
+        // Animate the bottom slide
+        document.getElementsByClassName("slide__backfigure")[0].classList.add("slide__backfigure--visible");
 
-                // Give them another CSS transition
-                setTimeout(() => coin.style.transition = "left .75s ease-in-out, top 2s ease-in-out", 2000);
-            }, 50 * i);
+        // Add all coins to array
+        coins[0].style.display = "block";
+        for (let i = 0; i < (amountOfSlides - 1); i++) {
+            let coin = app.appendChild(coins[0].cloneNode(true));
+            coin.style.display = "block";
+            coins.push(coin);
         }
-    }, 250);
-
-    setTimeout(() => {
-        let slideIndex = 0;
-
-        app.addEventListener("click", () => {
-            let slideColor = slideColors[slideIndex];
-
-            coins[slideIndex].style.left = -100 + "vw";
-            slides[0].style.marginLeft = ((slideIndex + 1) * -36) + "rem";
-            document.getElementsByClassName("slide__wrapper")[0].style.backgroundColor = slideColor.background;
-            document.getElementsByClassName("slide__backfigure")[0].style.backgroundColor = slideColor.main;
-
-            slideIndex++;
-
-            coinsWidth = (coins.length - slideIndex) * 4;
-            leftRemValue = (36 - coinsWidth) / 2 - 0.4;
-
-            for (let i = slideIndex; i < coins.length; i++) {
-                let coin = coins[i];
-                coin.style.left = leftRemValue + "rem";
-                leftRemValue += 4;
+    
+        // Calculate the left value so the coins are centered
+        let coinsWidth = amountOfSlides * 4;
+        let leftRemValue = (36 - coinsWidth) / 2 - 0.4;
+    
+        setTimeout(() => {
+            // Position every coin centered at the bottom of the screen
+            for (let i = 0; i < coins.length; i++) {
+                setTimeout(() => {
+                    let coin = coins[i];
+                    coin.style.top = 67 + "rem";
+                    coin.style.left = leftRemValue + "rem";
+            
+                    leftRemValue += 4;
+            
+                    // Let every second coin slide down so they are paired
+                    if (i % 2) coin.style.top = (parseInt(coin.style.top.slice(0, -3)) + 1.25) + "rem";
+    
+                    // Give them another CSS transition
+                    setTimeout(() => coin.style.transition = "left .75s ease-in-out, top 2s ease-in-out", 2000);
+                }, 50 * i);
             }
-        });
-    }, 1);
-}, {once : true});
+        }, 250);
+    
+        setTimeout(() => {
+            let slideIndex = 0;
+    
+            app.addEventListener("click", () => {
+                let slideColor = slideColors[slideIndex];
+    
+                coins[slideIndex].style.left = -100 + "vw";
+                slides[0].style.marginLeft = ((slideIndex + 1) * -36) + "rem";
+                document.getElementsByClassName("slide__wrapper")[0].style.backgroundColor = slideColor.background;
+                document.getElementsByClassName("slide__backfigure")[0].style.backgroundColor = slideColor.main;
+    
+                slideIndex++;
+    
+                coinsWidth = (coins.length - slideIndex) * 4;
+                leftRemValue = (36 - coinsWidth) / 2 - 0.4;
+    
+                for (let i = slideIndex; i < coins.length; i++) {
+                    let coin = coins[i];
+                    coin.style.left = leftRemValue + "rem";
+                    leftRemValue += 4;
+                }
+            });
+        }, 1);
+    }, 2750);
+});
 
-findTheTruth.addEventListener("submit", (e) => {
-    e.preventDefault();
+// findTheTruth.addEventListener("submit", (e) => {
+//     e.preventDefault();
 
-    let button = findTheTruth.getElementsByClassName("btn")[0];
-    button.style.transition = "translate .3s ease-in-out";
-    button.style.translate = "36rem 0";
-    findTheTruth.getElementsByClassName("search-glass")[0].style.bottom = "4.5rem";
+//     let button = findTheTruth.getElementsByClassName("btn")[0];
+//     button.style.transition = "translate .3s ease-in-out";
+//     button.style.translate = "36rem 0";
+//     findTheTruth.getElementsByClassName("search-glass")[0].style.bottom = "4.5rem";
 
     
-    for (let i = 0; i < findTheTruth.getElementsByClassName("statements")[0].children.length; i++) findTheTruth.getElementsByClassName("statements")[0].children[i].classList.add("statement--error")
-    findTheTruth.getElementsByClassName("statements")[0].children[1].classList.add("statement--check")
-});
+//     for (let i = 0; i < findTheTruth.getElementsByClassName("statements")[0].children.length; i++) findTheTruth.getElementsByClassName("statements")[0].children[i].classList.add("statement--error")
+//     findTheTruth.getElementsByClassName("statements")[0].children[1].classList.add("statement--check")
+// });
