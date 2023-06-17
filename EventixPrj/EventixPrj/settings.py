cee3640c512457a34be5f1854a3860b6338b2e2c
Ștefan Popescu <stefan.popescu@student.fontys.nl>
2023-06-17 15:18:53 +0200

    sss

--- EventixPrj/EventixPrj/settings.py
+++ EventixPrj/EventixPrj/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "EventixApp",
+    "EventixPrj.EventixApp",
 ]
 
 MIDDLEWARE = [
