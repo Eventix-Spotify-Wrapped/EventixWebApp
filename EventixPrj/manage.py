fd8d1cad3278c88f754e92e3a385f3c13d138042
Ștefan Popescu <stefan.popescu@student.fontys.nl>
2023-06-18 14:12:29 +0200

    ss

--- EventixPrj/manage.py
+++ EventixPrj/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "EventixPrj.EventixPrj.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "EventixPrj.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
